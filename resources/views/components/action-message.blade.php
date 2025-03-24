c930bccfbeb4f0af8e406496958914b9489fc4ae
Marcel Cunha <marcel.cunha.mc@gmail.com>
2025-03-24 19:56:49 -0300

    ajusta componente de toast

--- resources/views/components/action-message.blade.php
+++ resources/views/components/action-message.blade.php
@@ -1,10 +1,33 @@
-@props(['on'])
+@props(['type' => null])
+@php
+    $icon = match ($type) {
+        'success' => 'check-circle',
+        'danger' => 'exclamation-triangle',
+        'warning' => 'exclamation',
+        'info' => 'information-circle',
+        default => null,
+    };
 
-<div x-data="{ shown: true, timeout: null }"
-    x-init="@this.on('{{ $on }}', () => { clearTimeout(timeout); shown = true; timeout = setTimeout(() => { shown = false }, 2000);  })"
-    x-show.transition.out.opacity.duration.1500ms="shown"
-    x-transition:leave.opacity.duration.1500ms
-    style="display: none;"
-    {{ $attributes->merge(['class' => 'text-sm text-gray-600 dark:text-gray-400']) }}>
-    {{ $slot->isEmpty() ? 'Saved.' : $slot }}
+    $bgColor = match ($type) {
+        'success' => 'text-green-500',
+        'danger' => 'text-red-500',
+        'warning' => 'text-yellow-500',
+        'info' => 'text-blue-500',
+        default => 'text-gray-200',
+    };
+@endphp
+
+<div x-data="{ shown: true, timeout: null }" x-init="() => { clearTimeout(timeout); timeout = setTimeout(() => { shown = false }, 2000);  }" x-show.transition.out.opacity.duration.1500ms="shown"
+    x-transition:leave.opacity.duration.1500ms style="display: none;"
+    {{ $attributes->merge(['class' => 'bg-gray-200 flex py-1 gap-2 px-2 w-80  rounded-sm fixed top-20 right-4 z-10 text-sm  ']) }}>
+    @if (isset($icon))
+        <x-dynamic-component :component="'heroicon-o-' . $icon" class='w-15 {{ $bgColor }}' />
+    @endif
+
+
+    <div class='flex justify-center items-center'>{{ $slot }}</div>
+
+    <button class='flex justify-center items-center p-2  hover:cursor-pointer' @click="shown = false">
+        <x-heroicon-o-x-mark class='w-4 h-4 stroke-2 rounded-sm hover:bg-gray-300' />
+    </button>
 </div>
