5d51c716ee9ccfa2da97da58c2fb160241ffbb21
Marcel Cunha <marcel.cunha.mc@gmail.com>
2025-03-25 11:10:37 -0300

    adiciona configuração ao cadastro de equipamento

--- resources/views/pages/register/devices/create.blade.php
+++ resources/views/pages/register/devices/create.blade.php
@@ -1,20 +1,42 @@
 <x-pages.upsert title='Novo Equipamento'>
     <x-form action="{{ route('devices.store') }}" cancelRoute="{{ route('devices.index') }}">
-        <div class="grid lg:grid-cols-6 gap-x-4 gap-y-6">
+        <div x-data="{ type: 1 }">
+            <div class="grid lg:grid-cols-6 gap-x-4 gap-y-6">
+                <x-select value="{{ old('client_id') }}" label='Proprietário' parent-class='col-span-4' name='client_id'
+                    :options="$clients" placeholder='Selecione' />
+                <x-select value="{{ old('type_id') }}" label='Tipo' name='type_id' :options="$types" x-model='type'
+                    placeholder='Selecione' />
+                <x-select value="{{ old('brand_id') }}" label='Marca' name='brand_id' :options="$brands"
+                    placeholder='Selecione' />
+                <x-input-text value="{{ old('model') }}" parent-class='col-span-2' label='Modelo' name='model' />
+                <x-input-text value="{{ old('serial_number') }}" parent-class='col-span-2' label='Serial'
+                    name='serial_number' />
+                <x-input-text value="{{ old('service_tag') }}" parent-class='col-span-2' label='Tag de Serviço'
+                    name='service_tag' />
+                <x-textarea value="{{ old('description') }}" label='Descrição' parent-class='col-span-6'></x-textarea>
+            </div>
+            <template x-if="type == '1'">
+                <div class='mt-10'>
+                    <x-card label='Configurações' class='border border-gray-100'>
+                        <div class='grid lg:grid-cols-6 gap-x-4 gap-y-6' x-data='{memories:1, hds: 1}'>
+                            <x-input-text label='Sistema Operacional' />
+                            <x-select label='Marca da Placa Mãe' :options='$brands' />
+                            <x-input-text label='Modelo da Placa Mãe' />
+                            <x-select label='Marca do Processador' :options='$brands' />
+                            <x-input-text label='Modelo do Processador' />
+                            <x-select label='Marca da Placa de Vídeo' :options='$brands' />
+                            <x-input-text label='Modelo da Placa de Vídeo' />
+                            <template x-for="i in hds">
+                                <x-select label='Marca do HD' :options='$brands' />
+                                <x-input-text label='Modelo do HD' />
+                                <x-input-text label='Tamanho do HD' />
+                            </template>
+                            {{-- <x-select label='Marca da Memória RAM' :options='$brands' /> --}}
 
-            <x-select value="{{ old('client_id') }}" label='Proprietário' parent-class='col-span-4' name='client_id'
-                :options="$clients" placeholder='Selecione' />
-            <x-select value="{{ old('type_id') }}" label='Tipo' name='type_id' :options="$types"
-                placeholder='Selecione' />
-            <x-select value="{{ old('brand_id') }}" label='Marca' name='brand_id' :options="$brands"
-                placeholder='Selecione' />
-
-            <x-input-text value="{{ old('model') }}" parent-class='col-span-2' label='Modelo' name='model' />
-            <x-input-text value="{{ old('serial_number') }}" parent-class='col-span-2' label='Serial' name='serial_number' />
-            <x-input-text value="{{ old('service_tag') }}" parent-class='col-span-2' label='Tag de Serviço'
-                name='service_tag' />
-
-            <x-textarea value="{{ old('description') }}" label='Descrição' parent-class='col-span-6'></x-textarea>
+                        </div>
+                    </x-card>
+                </div>
+            </template>
         </div>
     </x-form>
 </x-pages.upsert>
