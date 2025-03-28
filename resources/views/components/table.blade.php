32b09a40af3bb779ecec457ab9b2239f23c1bec9
Marcel Cunha <marcel.cunha.mc@gmail.com>
2025-03-28 09:03:07 -0300

    adiciona funcionalidades ao componente de tabela

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -4,7 +4,7 @@
         <!-- Table header -->
         <thead class="text-xs uppercase text-gray-400 dark:text-gray-500 bg-gray-50 dark:bg-gray-700/50 rounded-xs">
             <tr>
-                @foreach ($header as $title)
+                @foreach ($getTitles() as $title)
                     <th class="p-2">
                         <div class="font-semibold text-left">{{ $title }}</div>
                     </th>
@@ -19,42 +19,49 @@
         </thead>
         <!-- Table body -->
         <tbody class="text-sm font-medium divide-y divide-gray-100 dark:divide-gray-700/60">
-            @forelse ($lines as $line)
+            @forelse ($rows as $row)
                 <tr>
-                    @foreach ($header as $name => $_)
+                    @foreach ($getColumns() as $column)
                         <!-- Row -->
                         <td class="p-2">
-                            {{ data_get($line, $name) }}
-                        </td>
-                    @endforeach
-                    @if (isset($actions))
-                        <td class="whitespace-nowrap">
-                            @if (array_key_exists('show', $actions))
-                                <x-button :href="route(data_get($actions, 'show'), $line->id)" class="h-7 w-7" color=secondary btn='btn-xs'>
-                                    <x-heroicon-o-eye class=" text-gray-300" />
-                                </x-button>
-                            @endif
-                            @if (array_key_exists('edit', $actions))
-                                <x-button :href="route(data_get($actions, 'edit'), $line->id)" class="h-7 w-7" color=secondary btn='btn-xs'>
-                                    <x-heroicon-o-pencil class=" text-gray-300" />
-                                </x-button>
-                            @endif
-                            @if (array_key_exists('delete', $actions))
-                                <x-button class="h-7 w-7" color=secondary btn='btn-xs'
-                                    @click="show=true;name='{{ $line->name }}';route='{{ route(data_get($actions, 'delete'), $line->id) }}'">
-                                    <x-heroicon-o-trash class="text-gray-300" />
-                                </x-button>
-                            @endif
-                        </td>
-                    @endif
-                </tr>
-            @empty
-                <tr>
-                    <td colspan='{{ isset($actions) ? count($header) + 1 : count($header) }}' class="p-2 text-center">Não há
-                        registros para esse recurso</td>
-                </tr>
-            @endforelse
-        </tbody>
-    </table>
+                            @if ($isCustomCell($column) && isset($cell))
+                                <!-- Se o slot 'cells' existir, renderiza o conteúdo customizado -->
+                                {{ $cell($row) }}
+                            @else
+                                <!-- Se não houver slot, exibe normalmente -->
+                                {{ data_get($row, $column, '') }}
+                            @endisset
+                    </td>
+                @endforeach
+                @if (isset($actions))
+                    <td class="whitespace-nowrap">
+                        @if (array_key_exists('show', $actions))
+                            <x-button :href="route(data_get($actions, 'show'), $row->id)" class="h-7 w-7" color=secondary btn='btn-xs'>
+                                <x-heroicon-o-eye class=" text-gray-300" />
+                            </x-button>
+                        @endif
+                        @if (array_key_exists('edit', $actions))
+                            <x-button :href="route(data_get($actions, 'edit'), $row->id)" class="h-7 w-7" color=secondary btn='btn-xs'>
+                                <x-heroicon-o-pencil class=" text-gray-300" />
+                            </x-button>
+                        @endif
+                        @if (array_key_exists('delete', $actions))
+                            <x-button class="h-7 w-7" color=secondary btn='btn-xs'
+                                @click="show=true;name='{{ $row->name }}';route='{{ route(data_get($actions, 'delete'), $row->id) }}'">
+                                <x-heroicon-o-trash class="text-gray-300" />
+                            </x-button>
+                        @endif
+                    </td>
+                @endif
+            </tr>
+        @empty
+            <tr>
+                <td colspan='{{ $totalColumns }}' class="p-2 text-center">
+                    Não há
+                    registros para esse recurso</td>
+            </tr>
+        @endforelse
+    </tbody>
+</table>
 
 </div>
